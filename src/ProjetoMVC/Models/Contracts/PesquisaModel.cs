6cfa4b1a99e3a9d30ddb22f193e2d4fbe60458c9
Arthur0006 <arthurrizzodutra@hotmail.com>
2023-11-16 09:03:09 -0300

    .....

--- src/ProjetoMVC/Models/Contracts/PesquisaModel.cs
+++ src/ProjetoMVC/Models/Contracts/PesquisaModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ProjetoMVC.Models.Contracts
+﻿using System.ComponentModel;
+
+namespace ProjetoMVC.Models.Contracts
 {
     public class PesquisaModel
     {
+        [DisplayName("Marca")]
         public int MarcaId { get; set; }
+        [DisplayName("Modelo")]
         public int ModeloId { get; set; }
         public string Pergunta { get; set; }
         public string? Resposta { get; set; }
